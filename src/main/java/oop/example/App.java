97fa3eebdb44f2acfee1904c8d6b18d89454dd6d
John Doe <bknudson117@gmail.com>
2021-05-30 22:39:59 -0400

    exercise2

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,22 @@
+// Takes user input and returns character count
+package oop.example;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        //Get input
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the input string? ");
+        String userInput = input.nextLine();
+
+        // Input Check
+        while(userInput.length() == 0){
+            System.out.println("Please input something into the program");
+            userInput = input.nextLine();
+        }
+        // Result
+        System.out.printf("%s has %d characters.", userInput, userInput.length());
+    }
+}
