f697ede6089123f0d2771730f5a23d8f10739624
Cryogenic117 <84353748+Cryogenic117@users.noreply.github.com>
2021-06-03 12:05:16 -0400

    Update App.java

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Brandon Knudson
+ */
+
 // Takes user input and returns character count
 package oop.example;
 import java.util.Scanner;
